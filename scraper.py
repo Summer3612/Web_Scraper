0f7a6f39b5674d17d79c26b2e77509b2d0bd5102
Summer3612 <summer913745@gmail.com>
2022-07-28 18:15:39 +0100

    compile methods for testing

--- scraper.py
+++ scraper.py
@@ -2,12 +2,14 @@
 # from cgitb import text
 # from lib2to3.pgen2 import driver
 # from os import link
+from importlib.resources import path
 import requests
 # from bs4 import BeautifulSoup
 import urllib.request
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
 import time
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
@@ -31,14 +33,14 @@ class Scraper:
         self.driver = webdriver.Chrome(options=chr_options)
 
         self.path = self.driver.get(self.URL)
-        self.delay = 5
+        self.delay = 1
         self.if_next_page=True
         
-        # Create target Directory
-       
+        # Create folder raw_data to save data later
         try:
-            dirName = 'raw data'
-            os.mkdir(dirName)
+            dirName = 'raw_data'
+            path = os.path.join('/Users/shubosun/Desktop/Data_Collection',dirName)
+            os.mkdir(path)
             print("Directory " , dirName ,  " Created ") 
         except FileExistsError:
             print("Directory " , dirName ,  " already exists")
@@ -53,10 +55,10 @@ class Scraper:
         except TimeoutException:
             print("Loading took too much time!")
 
-        # return self.driver 
+        
 
-    def _scroll(self):
-        # self.search()
+    def _scroll_to_bottom(self):
+        
         # Get scroll height
         last_height = self.driver.execute_script("return document.body.scrollHeight")
 
@@ -78,9 +80,9 @@ class Scraper:
         try: 
             search_bar= WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.XPATH, '//*[@name="search-term"]')))
             search_bar.send_keys(self.items_to_search)
-            time.sleep(0.8)
+            time.sleep(0.5)
             search_bar.send_keys(Keys.ENTER)
-            time.sleep(0.8)
+            time.sleep(0.5)
         
         except TimeoutException:
             
@@ -100,16 +102,10 @@ class Scraper:
             print("Loading took too much time! Seems this is the last page!")
             self.if_next_page=False
         
-    # private
+    
     def _find_all_links(self):
-        
-        self._accept_cookies()
-        time.sleep(0.8)
-        
-        self._search()
-        time.sleep(0.8)
 
-        self.link_list=[]
+        link_list=[]
         page=0
 
         while self.if_next_page==True: 
@@ -118,7 +114,7 @@ class Scraper:
             print(f'page {page}')
             
             # scroll down to the bottom for items to load
-            self._scroll()
+            self._scroll_to_bottom()
             time.sleep(0.5)
             
             # extract item link from the current page
@@ -126,53 +122,50 @@ class Scraper:
     
             for link in links:
                 print (link.get_attribute('href'))
-                self.link_list.append(link.get_attribute('href'))
+                link_list.append(link.get_attribute('href'))
             
             # go to next page if there is any 
             self._go_to_next_page()
             time.sleep(0.5)
         
-        print (len(self.link_list)) 
+        print (len(link_list)) 
+
+        return link_list
+ 
         
-    
-    def _retrieve_text_from_each_page(self):
-        
-        self.link = 'https://www.johnlewis.com/dune-santino-nubuck-woven-trim-loafers/p5546120'
-        self.driver.get(self.link)
-        
-        product_info_dic = {'uuid':str(uuid.uuid4()), 'product id': '', 'product name': '','product colour': '', 'product rating': '', 'available size and price':[], 'src links':[]}
-        
-        # get id from url e.g. https://www.johnlewis.com/dune-reid-waterproof-wellington-boots-black/p5403916
-        product_info_dic['product id']=self.link.split('/')[4]
-        
-        # get product name and colour from the heading 'h1' of the page
-        productName=WebDriverWait(self.driver,self.delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="column-right"]/section/div[1]/div[1]/h1')))
-        product_info_dic['product name']=productName.text.split(',')[0]
-        product_info_dic['product colour']=productName.text.split(',')[1]
-        
+    def _get_product_rating(self):   
         # get rating - some products' rating is not availabe so try is used.
+        rating = ''
         try:
             productRating=WebDriverWait(self.driver,self.delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="column-right"]/section/div[1]/div[2]/a/div/div[1]/div[2]/span')))
-            product_info_dic['product rating']=float(productRating.text.split(' ')[9])
+            rating=float(productRating.text.split(' ')[9])
         
         except TimeoutException: 
-            product_info_dic['product rating']='no rating available'
+            rating='no rating available'
+        
+        return rating
 
-        # different size has different availability and price so the below is created
+    # different size has different availability and price so the below is created in one method
+    def _get_product_size_availability_price_list(self): 
+        
         size_list = WebDriverWait(self.driver,self.delay).until(EC.presence_of_all_elements_located((By.XPATH, '//button[@data-cy="size-selector-item"]')))
         
         size_availability_price=[]
         
         for size in size_list:
             temp_list=[]
+            
             try:
                 size.click()
                 time.sleep(0.5)
-            except: 
-                pass
+                
+            except Exception as e: 
+                print(f'cannot click {size_list.index(size)}')
+                print(e.args)
             
             # the attribute 'aria-label' of size is in the format like '4. This size is selected' if available or ' 8. This size is selected but unavailable'
             # split is used to get separated information of size and whether it is available
+            
             size.get_attribute('aria-label').split('.')[0]
             temp_list.append(size.get_attribute('aria-label').split('.')[0])
 
@@ -186,18 +179,21 @@ class Scraper:
             temp_list.append(float(productPrice.text.split('£')[1]))
 
             # get discounted price if on sale
+            
             try: 
-                discount_price=WebDriverWait(self.driver,0.5).until(EC.presence_of_element_located((By.XPATH,'//*[@id="column-right"]/section/div[1]/div[2]/div/section[1]/span/span[2]/span')))
-                temp_list.append ('discount price '+discount_price.text)
+                discount_price=WebDriverWait(self.driver,self.delay).until(EC.presence_of_element_located((By.XPATH, '//span[@class="ProductPrice_price__niw6f ProductPrice_reduced__kIj_g"]/span[@role="text"]')))
+                temp_list.append (discount_price.get_attribute('aria-label'))
             except TimeoutException: 
                 temp_list.append ('no discount')
         
             size_availability_price.append(temp_list)
 
-        product_info_dic['available size and price']=size_availability_price
-   
+        return size_availability_price
+
         
-        # get src of the images of the product 
+   
+    def _get_product_src(self): 
+    # get src of the images of the product 
         src_elements = WebDriverWait(self.driver,self.delay).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@class="ImageMagnifier_image-wrapper__GhoSr"]')))
 
         product_src_list=[]
@@ -205,27 +201,79 @@ class Scraper:
             src = i.find_element(By.CSS_SELECTOR,'img')
             product_src_list.append(src.get_attribute('src'))
         
-        product_info_dic['src links'] = product_src_list
-
-        print (product_info_dic)
-        
-        return product_info_dic
-
-    def _save_image_locally(self):
-        info= self._retrieve_text_from_each_page()
-
-        for i in info['src links']:
-            # need to find the local path to save image but currently downloading is fine
-            urllib.request.urlretrieve(i,f"{info['product id']}_{info['src links'].index(i)}.jpg")
-
+        return product_src_list
 
     
+    def _save_product_data_in_json(self,product_info_dic): 
+
+        with open(f"/Users/shubosun/Desktop/Data_Collection/raw_data/{product_info_dic['product id']}/{ product_info_dic['product id']}.json","w") as f:
+            json.dump (product_info_dic,f, indent=2)
+        
+    
+    def _create_product_folder(self, product_info_dic):
+
+        try:
+            dirName = product_info_dic['product id']
+            path = os.path.join('/Users/shubosun/Desktop/Data_Collection/raw_data',dirName)
+            os.mkdir(path)
+            print("Folder for product " , dirName ,  " Created ") 
+        except FileExistsError:
+            print("Folder for product " , dirName ,  " already exists")
+    
+
+
+    def _save_image_locally(self,product_info_dic):
+
+        for i in product_info_dic['src links']:
+            # need to find the local path to save image but currently downloading is fine
+            urllib.request.urlretrieve(i,f"/Users/shubosun/Desktop/Data_Collection/raw_data/{ product_info_dic['product id']}/{ product_info_dic['product id']}_{ product_info_dic['src links'].index(i)}.jpg")
+
+    
+    def _get_prodcut_data(self,link):
+        
+        self.driver.get(link)
+
+        product_info_dic = {'uuid':str(uuid.uuid4()), 'product id': '', 'product name': '','product colour': '', 'product rating': '', 'available size and price':[], 'src links':[]}
+        
+        # get id from url e.g. https://www.johnlewis.com/dune-reid-waterproof-wellington-boots-black/p5403916
+        product_info_dic['product id']=link.split('/')[4]
+        
+        # get product name and colour from the heading 'h1' of the page
+        productName=WebDriverWait(self.driver,self.delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="column-right"]/section/div[1]/div[1]/h1')))
+        product_info_dic['product name']=productName.text.split(',')[0]
+        product_info_dic['product colour']=productName.text.split(',')[1]
+        product_info_dic['product rating'] = self._get_product_rating()
+        product_info_dic['available size and price']=self._get_product_size_availability_price_list()
+        product_info_dic['src links'] = self._get_product_src()
+
+        self._create_product_folder(product_info_dic)
+        self._save_product_data_in_json(product_info_dic)
+        self._save_image_locally(product_info_dic)
+
+        return product_info_dic
+    
+    def _trial(self):
+        self._accept_cookies()
+        time.sleep(0.5)
+        
+        self._search()
+        time.sleep(0.5)
+
+        link_list = self._find_all_links()
+        time.sleep(0.5)
+
+        self._get_prodcut_data(link_list[0])
+
+        self._get_prodcut_data('https://www.johnlewis.com/dune-chai-suede-chelsea-boots/p6108415')
+
+
+
 
         
 if __name__ =='__main__':
     url = 'https://www.johnlewis.com'
     item_to_search = 'dune shoes'
-    Scraper(url,item_to_search)._save_image_locally()
+    Scraper(url,item_to_search)._trial()
 
 
 
