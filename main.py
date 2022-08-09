61166ca2934c387843b2376dc51b3567730261b7
Summer3612 <summer913745@gmail.com>
2022-08-09 19:19:51 +0100

    updated code

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import scraper 
+from scraper.Scraper import JlScraper
 
 if __name__ =='__main__':
-    url = 'https://www.johnlewis.com'
-    item_to_search = 'dune shoes'
-    trial = scraper(url,item_to_search)
+    
+    john_lewis=JlScraper()
+    # john_lewis.accept_cookies()
+    # john_lewis.search('dune shoes')
+    # products=john_lewis.find_all_search_result_links()
+    product_dic=john_lewis.create_prodcut_dic('https://www.johnlewis.com/john-lewis-anyday-baby-flower-heart-sleepsuit-pack-of-3-pink-multi/p6202474')
+    john_lewis.save_product_info(product_dic)
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,8 +1,4 @@
-from math import prod
-from this import d
-from turtle import st
 import urllib.request
-from venv import create
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -16,13 +12,13 @@ import json
 from pandas import json_normalize
 import uuid 
 import os
-import pandas as pd
+
 
 # from urllib.request import BaseHandler
 
 class Scraper:
 
-    def __init__(self, URL: str):
+    def __init__(self, URL: str='https://www.johnlewis.com'):
         self.URL = URL
         
         # not to close window once the method is running
@@ -37,7 +33,7 @@ class Scraper:
     
     
     @staticmethod
-    def create_folder(folder_name,folder_path='/Users/shubosun/Desktop/Data_Collection/'):
+    def create_folder(folder_name,folder_path:str='/Users/shubosun/Desktop/'):
 
         try:
             path = os.path.join(folder_path,folder_name)
@@ -51,7 +47,7 @@ class Scraper:
         return path
     
     @staticmethod
-    def save_dic_in_json(dic_to_save:dict, file_name:str,folder_path='/Users/shubosun/Desktop/Data_Collection/raw_data/'): 
+    def save_dic_in_json(dic_to_save:dict, file_name:str,folder_path:str='/Users/shubosun/Desktop/'): 
 
         file=f"{folder_path}/{file_name}.json"
         with open(file,"w") as f:
@@ -59,38 +55,37 @@ class Scraper:
 
 
     @staticmethod
-    def download_image_locally(image_url:str, image_name:str, folder_path:str='/Users/shubosun/Desktop/Data_Collection/raw_data/'):
+    def download_image_locally(image_url:str, image_name:str, folder_path:str='/Users/shubosun/Desktop/'):
             # need to find the local path to save image but currently downloading is fine
         name=f"{folder_path}/{image_name}.jpg"
         urllib.request.urlretrieve(image_url,name)
 
     
-    def accept_cookies(self,path='//*[@data-test="allow-all"]'):
+    def accept_cookies(self,path:str='//*[@data-test="allow-all"]'):
     
         try:
             accept_cookies_button = WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.XPATH, path)))
             accept_cookies_button.click()
-            time.sleep(1)
         except TimeoutException:
-            print("Loading took too much time!")
+            print("Loading took too much time! Cookies are already accepted")
 
     
     # FIXME:it is not working. 
     def close_live_chat_box(self):
         
-        self.accept_cookies()
-        time.sleep(1)
+        # self.accept_cookies()
         try:
-
-            chat_box=self.driver.find_element((By.XPATH, '//*[@id="closeButtonId"]'))
+            chat_box= WebDriverWait(self.driver,self.delay).until(EC.presence_of_element_located((By.XPATH, '//span[@id="closeButtonId"]')))
             chat_box.click()
         
         except TimeoutException:
             print("Loading took too much time! Maybe there is no live chat box!")
 
+
     def scroll_down(self):
 
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight-2000);")
+
 
     
     def scroll_down_till_bottom(self):
@@ -99,7 +94,6 @@ class Scraper:
         This method is for website to keep scrolling down until the page is no longer loading. 
 
         """
-
         # Get scroll height
         last_height = self.driver.execute_script("return document.body.scrollHeight")
 
@@ -115,7 +109,7 @@ class Scraper:
                 break
             last_height = new_height
     
-    def search(self, item_to_search ='dune shoes', xpath = '//input[@name="search-term"]',):
+    def search(self, item_to_search:str, xpath:str = '//input[@name="search-term"]'):
         
         search_bar= WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.XPATH, xpath)))
         search_bar.send_keys(item_to_search)
@@ -124,25 +118,20 @@ class Scraper:
         time.sleep(0.5)
 
 
-    def go_to_next_page(self, xpath='//a[@aria-label="Next"]'):
+    def go_to_next_page(self, xpath:str='//a[@aria-label="Next"]'):
 
-        try: 
-            next= WebDriverWait(self.driver,self.delay).until(EC.presence_of_element_located((By.XPATH, xpath)))
-            next.click()
-        
-        except TimeoutException:
-            print("Loading took too much time! Seems this is the last page!")
-            self.if_next_page=False
+        next= WebDriverWait(self.driver,self.delay).until(EC.presence_of_element_located((By.XPATH, xpath)))
+        next.click()
     
-    def find_all_product_links(self):
+    def find_all_search_result_links(self)->list:
 
-        """This method is to get the link of all products after search"""
+        """This method is to get the link of all products of search result"""
 
         link_list=[]
         page=0
-        self.if_next_page=True
+    
 
-        while self.if_next_page: 
+        while True: 
             # page number
             page +=1
             print(f'page {page}')
@@ -159,20 +148,37 @@ class Scraper:
                 link_list.append(link.get_attribute('href'))
             
             # go to next page if there is any 
-            self.go_to_next_page()
-            time.sleep(0.5)
+            try: 
+                self.go_to_next_page()
+                time.sleep(0.5)
+         
+            except TimeoutException:
+                print("Loading took too much time! Seems this is the last page!")
+                break
         
         print (len(link_list)) 
 
         return link_list
 
-    def get_product_rating(self, xpath ='//div[@class="rating_starRated__tfqFS"]'):    
+class JlScraper(Scraper):
+
+    def get_product_id(self, xpath:str = '//jl-store-stock')->str:
+        name = WebDriverWait(self.driver,self.delay).until(EC.presence_of_element_located((By.XPATH, xpath)))
+        product_id = name.get_attribute('skuid')
+        return product_id
+    
+    # FIXME: only certain categories of products are fine
+    def get_product_name(self,xpath:str = '//div[@class="xs-up"]//*[@class="ProductTitle_title__JiefQ"]')->str:
+        productName=WebDriverWait(self.driver,self.delay).until(EC.presence_of_element_located((By.XPATH, xpath)))
+        return productName.text
+
+
+    def get_product_rating(self, xpath:str ='//span[@data-test="rating"]')->str:    
         # get rating - some products' rating is not availabe so try is used.
         
         rating = ''
         try:
             productRating=WebDriverWait(self.driver,self.delay).until(EC.presence_of_all_elements_located((By.XPATH, xpath)))
-            
             rating=productRating[1].text
         
         except TimeoutException: 
@@ -182,10 +188,11 @@ class Scraper:
 
     # FIXME: only certain categories of products are fine
     # different size has different availability and price so the below is created in one method
-    def get_product_size_availability_price_list(self,xpath ='//button[@data-cy="size-selector-item"]'): 
+    def get_product_size_availability_price_list(self,xpath:str ='//button[@data-cy="size-selector-item"]')->list: 
+        self.accept_cookies()
         
         size_list = WebDriverWait(self.driver,self.delay).until(EC.presence_of_all_elements_located((By.XPATH, xpath)))
-        
+
         size_availability_price=[]
 
         for size in size_list:
@@ -209,23 +216,14 @@ class Scraper:
             else: 
                 temp_list.append('available')
             
-            
-            # get original price
-            productPrice=WebDriverWait(self.driver,self.delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="column-right"]/section/div[1]/div[2]/div/section[1]/span/span/span')))
-            temp_list.append(float(productPrice.text.split('£')[1]))
+            temp_list.append(self.get_product_price_history())
 
-            # get discounted price if on sale
-            try: 
-                discount_price=WebDriverWait(self.driver,self.delay).until(EC.presence_of_element_located((By.XPATH, '//span[@class="ProductPrice_price__niw6f ProductPrice_reduced__kIj_g"]/span[@role="text"]')))
-                temp_list.append (discount_price.get_attribute('aria-label'))
-            except TimeoutException: 
-                temp_list.append ('no discount')
-        
+
             size_availability_price.append(temp_list)
 
         return size_availability_price
    
-    def get_product_src(self,xpath='//*[@class="ImageMagnifier_image-wrapper__GhoSr"]')->list: 
+    def get_product_src(self,xpath:str='//*[@class="ImageMagnifier_image-wrapper__GhoSr"]')->list: 
         # get src of the images of the product 
         src_elements = WebDriverWait(self.driver,self.delay).until(EC.presence_of_all_elements_located((By.XPATH, xpath)))
 
@@ -237,19 +235,20 @@ class Scraper:
         return product_src_list
     
     # # FIXME: only certain categories of products are fine
-    def get_product_id(self, xpath = '//jl-store-stock')->str:
-        name = WebDriverWait(self.driver,self.delay).until(EC.presence_of_element_located((By.XPATH, xpath)))
-        product_id = name.get_attribute('skuid')
-        return product_id
     
-    # FIXME: only certain categories of products are fine
-    def get_product_name(self,xpath='//*[@id="column-right"]/section/div[1]/div[1]/h1'):
-        productName=WebDriverWait(self.driver,self.delay).until(EC.presence_of_element_located((By.XPATH, xpath)))
-        return productName.text
 
-    def create_prodcut_dic(self)->dict:
+    def get_product_price_history(self,xpath:str='//span[@class="ProductPrice_prices-list__jbkRS"]')->str:
+        # price_element = WebDriverWait(self.driver,self.delay).until(EC.presence_of_element_located((By.XPATH,class_name)))
+        price_elements =self.driver.find_elements(By.XPATH,xpath)
+        # price=""
+        # for element in price_elements:
+        #     print (element.text)
+        return price_elements[1].text
 
-        self.accept_cookies()
+    def create_prodcut_dic(self,url:str)->dict:
+        self.driver.get(url)
+        time.sleep(1)
+        self.close_live_chat_box()
     
         product_info_dic = {'uuid':str(uuid.uuid4()), 'product id': '', 'product name': '', 'product rating': '', 'available size and price':[], 'src links':[]}
         
@@ -259,9 +258,14 @@ class Scraper:
         product_info_dic['available size and price']=self.get_product_size_availability_price_list()
         product_info_dic['src links'] = self.get_product_src()
 
+
+        return product_info_dic
+
+    def save_product_info(self,product_info_dic:dict):
+
         raw_data_folder_path= self.create_folder('raw data', '/Users/shubosun/Desktop/')
         product_folder_path = self.create_folder(product_info_dic['product id'],raw_data_folder_path)
-        self.save_dic_in_json(product_info_dic, product_info_dic['product id'],product_folder_path)
+        
         
         for src_link in product_info_dic['src links']:
             # need to find the local path to save image but currently downloading is fine
@@ -270,19 +274,13 @@ class Scraper:
 
         
         # df=pd.DataFrame.from_dict(product_info_dic, orient='index')
-
+        self.save_dic_in_json(product_info_dic, product_info_dic['product id'],product_folder_path)
         df=json_normalize(product_info_dic)
         print(df)
-        print (product_info_dic['src links'])
 
-        return product_info_dic
-    
 
         
-if __name__ =='__main__':
-    url = 'https://www.johnlewis.com'
-    Scraper('https://www.johnlewis.com/dune-eddris-leather-star-trainers-white/p6285361').create_prodcut_dic()
 
 
-    # df = pd.read_json('/Users/shubosun/Desktop/Data_Collection/raw_data/p6147654/p6147654.json')
+  
     
